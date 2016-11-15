53c1600e5304b650596cf285f7087a7f3bd2ba6d
amurali9 <abhinavm90@gmail.com>
2016-11-14 23:44:37 -0500

    file_shard

--- src/file_shard.h
+++ src/file_shard.h
@@ -8,10 +8,7 @@
 /* CS6210_TASK: Create your own data structure here, where you can hold information about file splits,
      that your master would use for its own bookkeeping and to convey the tasks to the workers for mapping */
 struct FileShard {
-   map<string,vector<int>> filemap;		//STL Map for File Split : filename + Offset(s)
-   vector<string> ip_filenames;			//Input filenames
-   int map_kB;
-   int n_fileshard;
+
 };
 
 
