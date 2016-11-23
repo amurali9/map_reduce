d2a0a55ec744d056cb523640282ee7427a87ee7b
Abhishek Sen <abhishekpsen@gmail.com>
2016-11-23 10:46:32 -0500

    Update master.h

--- src/master.h
+++ src/master.h
@@ -3,8 +3,25 @@
 #include "mapreduce_spec.h"
 #include "file_shard.h"
 
+<<<<<<< HEAD
+#include <grpc++/grpc++.h>
 using namespace std;
+#include "masterworker.grpc.pb.h"
 
+//grpc client
+using grpc::Channel;
+using grpc::ClientAsyncResponseReader;
+using grpc::ClientContext;
+using grpc::CompletionQueue;
+using grpc::Status;
+
+//Masterclient
+using masterworker::FileChunk;
+using masterworker::MapStatus;
+using masterworker::MasterWorker;
+
+
+using namespace std;
 /* CS6210_TASK: Handle all the bookkeeping that Master is supposed to do.
 	This is probably the biggest task for this project, will test your understanding of map reduce */
 class Master {
@@ -25,11 +42,49 @@ class Master {
 /* CS6210_TASK: This is all the information your master will get from the framework.
 	You can populate your other class data members here if you want */
 Master::Master(const MapReduceSpec& mr_spec, const std::vector<FileShard>& file_shards) {
-
 }
+
+class MasterClient {
+ public:
+  explicit MasterClient(std::shared_ptr<Channel> channel)
+      : stub_(MasterWorker::NewStub(channel)) {}
+
+  bool DoMap() {
+    // Data we are sending to the server.
+    FileChunk request;
+    request.set_name("dummy_file_shard");
+    MapStatus reply;
+    ClientContext context;
+    CompletionQueue cq;
+    Status status;
+
+    std::unique_ptr<ClientAsyncResponseReader<MapStatus> > rpc(
+        stub_->AsyncDoMap(&context, request, &cq));
+
+    rpc->Finish(&reply, &status, (void*)1);
+    void* got_tag;
+    bool ok = false;
+    GPR_ASSERT(cq.Next(&got_tag, &ok));
+    GPR_ASSERT(got_tag == (void*)1);
+    GPR_ASSERT(ok);
+    if (status.ok()) {
+      cout<<"RPC Done"<<endl;
+    } else {
+			cout<<"RPC failed"<<endl;
+    }
+		return status.ok();
+  }
+ private:
+  std::unique_ptr<MasterWorker::Stub> stub_;
+};
 
 
 /* CS6210_TASK: Here you go. once this function is called you will complete whole map reduce task and return true if succeeded */
 bool Master::run() {
+	//make rpc call to run the worker
+	MasterClient masterClient(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+	cout<<"Requesting worker to domap"<<endl;
+	bool reply = masterClient.DoMap();
+	cout<<reply;
 	return true;
 }
